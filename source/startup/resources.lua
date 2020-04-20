714a2ea65401b766fabc49b9de7ef87efce85f43
zetoinas <jose.sousa.franco@tecnico.ulisboa.pt>
2020-04-20 19:34:04 +0100

    new image

---
+++ source/startup/resources.lua
@@ -0,0 +1,15 @@
+sprites = {}
+
+sprites.beat = love.graphics.newImage("images/beat.png")
+sprites.cube = love.graphics.newImage("images/Cube.png")
+sprites.player = love.graphics.newImage("images/player.png")
+sprites.health_and_mojo = love.graphics.newImage("images/HealthAndMojo.png")
+sprites.hud_bit = love.graphics.newImage("images/hud_bit.png")
+sprites.shop = love.graphics.newImage("images/lojaMDJ.png")
+sprites.goblin = love.graphics.newImage("images/goblin.png")
+
+fonts = {}
+
+fonts.title = love.graphics.newFont("fonts/russoone/RussoOne-Regular.ttf", 42)
+fonts.debug = love.graphics.newFont("fonts/vt323/VT323-Regular.ttf", 20)
+fonts.origin = love.graphics.newFont("fonts/vt323/VT323-Regular.ttf", 78)
